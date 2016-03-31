f9ff62df038d90df376cdb15b427c2dfc8fcf91b
katsuhisa <code1110g-show@hotmail.co.jp>
2016-03-31 22:21:44 +0200

    urgently need to have code organizing photos

--- static/js/bkgchange.js
+++ static/js/bkgchange.js
@@ -10,21 +10,28 @@ $( '#bkgchange' ).click(function() {
     "../../static/gallary/suzu8.jpg",
     "../../static/gallary/suzu9.jpg",
     "../../static/gallary/suzu10.jpg",
-    "../../static/gallary/suzu10.jpg",
-    "../../static/gallary/suzu11.jpg",
-    "../../static/gallary/suzu12.jpg",
-    "../../static/gallary/suzu13.jpg",
     "../../static/gallary/suzu14.jpg",
     "../../static/gallary/suzu15.jpg",
     "../../static/gallary/suzu16.jpg",
-    "../../static/gallary/me1.jpg",
-    "../../static/gallary/me3.jpg",
+    "../../static/gallary/suzu17.jpg",
+    "../../static/gallary/suzu18.jpg",
+    "../../static/gallary/suzu19.jpg",
+    "../../static/gallary/suzu21.jpg",
+    "../../static/gallary/suzu22.jpg",
+    "../../static/gallary/suzu23.jpg",
+    "../../static/gallary/suzu24.jpg",
+    "../../static/gallary/masa1.jpg",
+    "../../static/gallary/masa2.jpg",
+    "../../static/gallary/masa3.jpg",
+    "../../static/gallary/masa4.jpg",
+    "../../static/gallary/masa5.jpg",
+    "../../static/gallary/masa6.jpg",
     ];
     var l = array.length;
     var r = Math.floor(Math.random()*l);
     var imgurl = array[r];
     $("body").css({
-      'background-size': "100%",
+      'background-size': "cover",
       'background-attachment': "fixd",
       'background-image': ('url("'+imgurl+'")')});
 
