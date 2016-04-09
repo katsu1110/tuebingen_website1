6781cbc868450146c1b2e32acac7d2d7dc2423d5
katsuhisa <code1110g-show@hotmail.co.jp>
2016-04-09 18:25:53 +0200

    silver background

--- top_page/views.py
+++ top_page/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from django.utils import timezone
 from top_page.models import Whatsnew, Upcoming
 
 # Create your views here.
 def index(request):
     whatsnews = Whatsnew.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:4]
-    upcomings = Upcoming.objects.all()[:4]
+    upcomings = Upcoming.objects.order_by('event_date')[:4]
     context = {'upcomings':upcomings,'whatsnews': whatsnews}
     return render(request, 'top_page/index.html', context)
 
