849100871b5bec54557a4b9b8ec99e38d2873821
katsuhisa <code1110g-show@hotmail.co.jp>
2016-04-15 19:31:16 +0200

    auto arrange upcoming event

--- top_page/views.py
+++ top_page/views.py
@@ -5,7 +5,7 @@ from top_page.models import Whatsnew, Upcoming
 # Create your views here.
 def index(request):
     whatsnews = Whatsnew.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:4]
-    upcomings = Upcoming.objects.order_by('event_date')[:4]
+    upcomings = Upcoming.objects.filter(event_date__gt=timezone.now()).order_by('event_date')[:4]
     context = {'upcomings':upcomings,'whatsnews': whatsnews}
     return render(request, 'top_page/index.html', context)
 
